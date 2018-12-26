e1593c8b1eb6e079cd8c6db21cb42db160305cd6
HeyKefka <46166339+HeyKefka@users.noreply.github.com>
2018-12-26 20:16:13 +0300

    Add files via upload

---
+++ main (1).cpp
@@ -0,0 +1,24 @@
+#include"game.h"
+
+
+
+int main()
+{
+
+	Game* game=new Game();
+
+	
+	game->setup();
+	game->difficulLevel();
+	game->initialize();
+
+	while (game->frame());
+
+	game->shutdown();
+
+
+	delete game;
+
+	return 0;
+}
+
